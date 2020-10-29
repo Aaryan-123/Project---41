1133070ef3af7bf2569e1f995436bf8fec82cb9f
Aaryan <70795502+Aaryan-123@users.noreply.github.com>
2020-10-29 19:34:49 +0530

    Update Umrella.js

--- Umrella.js
+++ Umrella.js
@@ -3,7 +3,7 @@ class Umbrella {
         var options = {
             isStatic : true
         }
-        this.image = loadImage("images/WalkingFrame");
+        this.image = loadImage("WalkingFrame");
 
         this.body = Bodies.circle(x,y,options);
         World.add(this.body);
